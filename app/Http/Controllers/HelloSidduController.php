e47072f3d67675aa714527b26e2c85af9b4932c2
siddu <siddarthvarmadongari@gmail.com>
2024-11-22 17:27:54 +0530

    first commit

---
+++ app/Http/Controllers/HelloSidduController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloSidduController extends Controller
+{
+    public function index()
+{
+return 'Hello, siddu!';
+}
+}
